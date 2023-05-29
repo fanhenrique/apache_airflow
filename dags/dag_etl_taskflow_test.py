a03b8981473cdab544f01554b479420aa72e7528
fanhenrique <fanhenrique@gmail.com>
2023-05-29 17:59:27 -0300

    dag etl taskflow test

---
+++ dags/dag_etl_taskflow_test.py
@@ -0,0 +1,39 @@
+import json
+
+from airflow.decorators import dag, task
+
+from datetime import datetime, timedelta
+
+# ETL TaskFlow test
+@dag(
+  start_date=datetime(2023,1,28),
+  schedule=timedelta(minutes=5),
+  catchup=False,
+  )
+def etl_taskflow():
+
+  @task()
+  def extract():
+
+    data = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
+    return json.loads(data)
+
+  @task()
+  def transform(data_dict: dict):
+    data = data_dict.values()
+    total_values = 0   
+    for value in data:
+      total_values += value
+
+    return {'total_values': total_values}
+
+  @task()
+  def load(total_values: float):
+    
+    print(f'Total values: {total_values}')  
+
+  data = extract()
+  data_transformed = transform(data)
+  load(data_transformed)
+
+etl_taskflow()
