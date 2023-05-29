0ea3dced7cba7c71dbbbcfb86238a3254985020d
fanhenrique <fanhenrique@gmail.com>
2023-05-29 17:47:55 -0300

    etl test

---
+++ dags/dag_etl_test.py
@@ -0,0 +1,52 @@
+import json
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+from datetime import datetime, timedelta
+
+def extract():
+
+  data = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
+  return json.loads(data)
+
+def transform(ti):
+
+  data = ti.xcom_pull(task_ids = 'extract')
+
+  total_values = 0   
+  for value in data.values():
+    total_values += value
+
+  return {'total_values': total_values}
+
+def load(ti):
+  
+  total_values = ti.xcom_pull(task_ids = 'transform')
+  print(f'Total values: {total_values}')
+
+# test ETL 
+with DAG(
+  'etl_test',
+  start_date=datetime(2023,1,28),
+  schedule=timedelta(minutes=5),
+  catchup=False,
+  ) as dag:
+
+  task_extract = PythonOperator(
+    task_id = 'extract',
+    python_callable=extract
+  )
+  
+  task_transform = PythonOperator(
+    task_id = 'transform',
+    python_callable=transform
+  ) 
+  
+  task_load = PythonOperator(
+    task_id = 'load',
+    python_callable=load
+  )
+
+  task_extract >> task_transform >> task_load
