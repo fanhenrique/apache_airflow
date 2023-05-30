17a3da7ab93b8111068ce237d38322fd042f0ab5
fanhenrique <fanhenrique@gmail.com>
2023-05-30 16:19:07 -0300

    reusing tasks

---
+++ dags/dag_reusing_task_import.py
@@ -0,0 +1,13 @@
+from dag_reusing_task import add_task
+from airflow.decorators import dag
+from datetime import datetime
+
+
+@dag(start_date=datetime(2023, 5, 27))
+def use_add_task():
+    start = add_task.override(priority_weight=3)(1, 2)
+    for i in range(3):
+        start >> add_task.override(task_id=f"new_add_task_{i}", retries=4)(start, i)
+
+
+created_dag = use_add_task()
