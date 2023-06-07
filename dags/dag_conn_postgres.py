7d2637f7a9d3cb7301768ba9eb34dd3d28f95035
fanhenrique <fanhenrique@gmail.com>
2023-06-07 19:01:33 -0300

    create insert

--- dags/dag_conn_postgres.py
+++ dags/dag_conn_postgres.py
@@ -4,18 +4,27 @@ from datetime import datetime, timedelta
 
 @dag(start_date=datetime(2023, 6, 6), schedule=timedelta(minutes=1), catchup=False)
 def create_tables():
+  
   create_user_table = PostgresOperator(
     task_id = 'create_table',
     postgres_conn_id = 'conn_postgres',
     sql = """
       CREATE TABLE IF NOT EXISTS user_table (
-        "id" INTEGER PRIMARY KEY,
-        "name" VARCHAR(100),
-        "login" VARCHAR(100),
-        "password" VARCHAR(100)
+        ID INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        name VARCHAR(100),
+        date date
       );""",
   )
  
+  insert_user = PostgresOperator(
+    task_id = 'insert_user',
+    postgres_conn_id = 'conn_postgres',
+    sql = """
+      INSERT INTO user_table (name, date)
+      VALUES ('joao', CURRENT_DATE);
+      """,
+  )
 
+  create_user_table >> insert_user
   
 create_tables()
