da748bbc029c09d96bbf64d0bebb691f74b1961f
fanhenrique <fanhenrique@gmail.com>
2023-06-07 19:20:12 -0300

    splited in sql file

--- dags/dag_conn_postgres.py
+++ dags/dag_conn_postgres.py
@@ -2,29 +2,26 @@ from airflow.decorators import task, dag
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime, timedelta
 
-@dag(start_date=datetime(2023, 6, 6), schedule=timedelta(minutes=1), catchup=False)
+@dag(
+  start_date=datetime(2023, 6, 6),
+  schedule=timedelta(minutes=1),
+  catchup=False,
+  template_searchpath='/opt/airflow/sql'  
+)
 def create_tables():
   
-  create_user_table = PostgresOperator(
+  create_user = PostgresOperator(
     task_id = 'create_table',
     postgres_conn_id = 'conn_postgres',
-    sql = """
-      CREATE TABLE IF NOT EXISTS user_table (
-        ID INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-        name VARCHAR(100),
-        date date
-      );""",
+    sql = 'create_user.sql',
   )
  
   insert_user = PostgresOperator(
     task_id = 'insert_user',
     postgres_conn_id = 'conn_postgres',
-    sql = """
-      INSERT INTO user_table (name, date)
-      VALUES ('joao', CURRENT_DATE);
-      """,
+    sql = 'insert_user.sql',
   )
 
-  create_user_table >> insert_user
+  create_user >> insert_user
   
 create_tables()
---
+++ sql/insert_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO user_table (name, date)
+  VALUES ('joao', CURRENT_DATE);
